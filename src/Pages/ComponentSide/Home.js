5008cefc7e24549520d112297da32de1b8af6ba5
Your Name Main <you@example.com>
2022-12-26 09:54:03 -0800

    sidebar icon added

--- src/Pages/ComponentSide/Home.js
+++ src/Pages/ComponentSide/Home.js
@@ -14,23 +14,25 @@ const Home = () => {
                  </div>
              </div>
              <div className='mt-10'>
-                <h3 className='text-2xl mb-1 flex items-center justify-center'> <MdDashboard className='mr-4'/> Dashboard</h3>
-                <h3 className='text-2xl mb-1 flex items-center justify-center'><MdOutlineInventory2 className='mr-8'/> Inventory</h3>
+                <h3 className='text-2xl mb-8 flex items-center justify-center'> <MdDashboard className='mr-4'/> Dashboard</h3>
+               <div className='bg-[#ecf5ec] '>
+               <h3 className='text-2xl text-[#8dee92] font-semibold mb-8 flex items-center justify-center'><MdOutlineInventory2 className='mr-8 text-[#8dee92]'/> Inventory</h3>
+               </div>
 
-                <h3 className='text-2xl mb-1 flex items-center justify-center'><MdOutlineHome className='mr-2'/> Marketplace</h3>
+                <h3 className='text-2xl mb-8 flex items-center justify-center'><MdOutlineHome className='mr-2'/> Marketplace</h3>
 
-                <h3 className='text-2xl mb-1 flex items-center justify-center'> <RiAuctionLine className='mr-6' /> Auction</h3>
+                <h3 className='text-2xl mb-8 flex items-center justify-center'> <RiAuctionLine className='mr-14' /> Auction</h3>
 
-                <h3 className='text-2xl mb-1 flex items-center justify-center'> <MdBorderColor className='mr-6' /> Orders</h3>
+                <h3 className='text-2xl mb-8 flex items-center justify-center'> <MdBorderColor className='mr-16' /> Orders</h3>
 
-                <h3 className='text-2xl mb-1 flex items-center justify-center'> <MdOutlineEmojiTransportation className='mr-6'/> Transports</h3>
+                <h3 className='text-2xl mb-8 flex items-center justify-center'> <MdOutlineEmojiTransportation className='mr-6'/> Transports</h3>
 
-                <h3 className='text-2xl mb-1 flex items-center justify-center'> <MdHourglassBottom className='mr-4'/> Laboratories</h3>
+                <h3 className='text-2xl mb-8 flex items-center justify-center'> <MdHourglassBottom className='mr-3'/> Laboratories</h3>
 
-                <h3 className='text-2xl mb-1 flex items-center justify-center'><BsArrowLeftRight 
+                <h3 className='text-2xl mb-8 flex items-center justify-center'><BsArrowLeftRight 
                  className='mr-4'/> Transaction</h3>
 
-                <h3 className='text-2xl mb-1 flex items-center justify-center'> <MdOutlineSettings  className='mr-4'/> Settings</h3>
+                <h3 className='text-2xl mb-8 flex items-center justify-center'> <MdOutlineSettings  className='mr-12'/> Settings</h3>
                 
              </div>
         </div>
